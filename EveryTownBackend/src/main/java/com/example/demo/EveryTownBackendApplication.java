11acde78600e45c2933857aa767136c56f72e5e9
delta <h0568@naver.com>
2023-01-12 11:02:11 +0900

    first commit

---
+++ EveryTownBackend/src/main/java/com/example/demo/EveryTownBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryTownBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EveryTownBackendApplication.class, args);
+	}
+
+}
